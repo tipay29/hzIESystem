b1f1854198fd6d22ad54a935e27945480f9148b7
Warren Hernandez <jwarren.hernandez@gmail.com>
2022-05-14 10:14:44 +0700

    lang add

--- resources/views/cut/index.blade.php
+++ resources/views/cut/index.blade.php
@@ -14,17 +14,17 @@
                             @can('create', App\Models\Cut::class)
                             <a href="{{route('cuts.create')}}"
                                class="btn btn-outline-success">
-                                {{__('text.Add').' '.__('text.Cut')}} </a>
+                                {{__('text.AddCuttingReport')}} </a>
                             @endcan
                             @can('viewUtil', App\Models\Cut::class)
                             <a href="{{route('cuts.util')}}"
                                class="btn btn-outline-success">
-                                {{__('text.Cut').' '.__('text.Util')}} </a>
+                                {{__('CuttingReport1')}} </a>
                             @endcan
                             @can('viewTotalUtil',App\Models\Cut::class)
                                     <a href="{{route('cuts.total-util')}}"
                                        class="btn btn-outline-success">
-                                        {{__('text.Total').' '.__('text.Cut') .' '. __('text.Util')}} </a>
+                                        {{__('CuttingReport2')}} </a>
                             @endcan
 
                         </div>
@@ -48,9 +48,9 @@
                                         <th width="10%" scope="col">{{__('text.Style')}}</th>
                                         <th width="15%" scope="col">{{__('text.PurchaseOrder')}}</th>
                                         <th width="5%" scope="col">{{__('text.Building')}}</th>
-                                        <th width="5%" scope="col">{{__('text.Table')}}</th>
-                                        <th width="18%" scope="col">{{__('text.Spread').' '. __('text.Date') .''. __('text.Time')}}</th>
-                                        <th width="18%" scope="col">{{__('text.Cut').' '. __('text.Date') .''. __('text.Time')}}</th>
+                                        <th width="5%" scope="col">{{__('text.Table')}}#</th>
+                                        <th width="18%" scope="col">{{__('SpreadStart(date/time)')}}</th>
+                                        <th width="18%" scope="col">{{__('CutStart(date/time)')}}</th>
                                         <th width="15%" scope="col">{{__('text.User')}}</th>
                                         @can('create', App\Models\Cut::class)
                                         <th width="4%" scope="col">{{__('text.Option')}}</th>
